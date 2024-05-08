fbfb310d9868e6a10920cec33260869bf9efb06d
Luis Martins <luismwolf@gmail.com>
2024-05-08 17:26:20 -0400

    change dockerfile react version

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_27_154737) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.boolean "published"
+    t.boolean "published", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
